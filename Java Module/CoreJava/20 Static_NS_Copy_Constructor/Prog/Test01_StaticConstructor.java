4cd7844c7fa61ab1fa89b22e1c930a7740153096
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-11-19 11:08:57 +0530

    static constructor

---
+++ Java Module/CoreJava/20 Static_NS_Copy_Constructor/Prog/Test01_StaticConstructor.java
@@ -0,0 +1,23 @@
+/**
+static constructor
+======================
+ - A constructor that is declared with the keyword static 
+   is called static constructor 
+   
+ - Java doesn't support static constructor, 
+   it leads to compile time error 'modifier static not allowed here' 
+   
+ - The static constructor is allowed in 'C#.NET' language  
+ 
+ - In Java, the static consturctor in nothing but 'static block'
+   which is used for initializing static variables
+ 
+*/
+class Example {
+                        
+		// //static constructor
+		// static Example() {   //CE: modifier staic not allowed here
+
+		// }
+
+}
