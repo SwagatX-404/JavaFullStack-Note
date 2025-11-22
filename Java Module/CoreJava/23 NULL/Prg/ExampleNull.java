228e0246d1d7a728504ddadbdac935b94ac61881
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-11-22 20:54:35 +0530

    null

---
+++ Java Module/CoreJava/23 NULL/Prg/ExampleNull.java
@@ -0,0 +1,25 @@
+class ExampleNull{
+    static int a = 10;
+    static int b = 20;
+
+    int x = 30;
+    int y = 40;
+
+
+    public static void main(String[] args) {
+        ExampleNull e1 = new ExampleNull();
+        ExampleNull e2 = new ExampleNull();
+      //  ExampleNull e3 =  null;
+
+       // System.out.println(e1.a);
+        System.out.println(e1.x);
+
+      //  System.out.println(e2.a);
+        System.out.println(e2.x);
+
+       // System.out.println(e3.a);
+       // System.out.println(e3.x);
+
+    }
+
+}
